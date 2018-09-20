96ef2b13e80b29d657ac2b47e791132f0cdca8a6
Duarte Pinto <duartemr.pinto@gmail.com>
2018-09-20 16:09:39 +0100

    finished until P09

--- src/main/scala/s99/ListsSolutions.scala
+++ src/main/scala/s99/ListsSolutions.scala
@@ -1,15 +1,70 @@
 package s99
 
 trait ListsSolutions {
-  def last[T](list: List[T]): T = ???
-  def penultimate[T](list: List[T]): T = ???
-  def nth[T](n: Int, list: List[T]): T = ???
-  def length[T](list: List[T]): Int = ???
-  def reverse[T](list: List[T]): List[T] = ???
-  def isPalindrome[T](list: List[T]): Boolean = ???
-  def flatten(list: List[Any]): List[Any] = ???
-  def compress[T](list: List[T]): List[T] = ???
-  def pack[T](list: List[T]): List[List[T]] = ???
+  def last[T](list: List[T]): T = list match{
+    case x :: Nil => x
+    case _ :: xs => last(xs)
+    case Nil => throw new NoSuchElementException
+  }
+
+  def penultimate[T](list: List[T]): T = list match {
+    case x :: _ :: Nil => x
+    case _ :: xs => penultimate(xs)
+    case _ => throw new NoSuchElementException
+  }
+
+  def nth[T](n: Int, list: List[T]): T = 
+    if (n == 0 ) list.head
+    else nth (n-1, list.tail)
+
+  def length[T](list: List[T]): Int = list match {
+    case Nil => 0
+    case x :: xs => length(xs) + 1
+  }
+
+  def reverse[T](list: List[T]): List[T] = list match {
+    case Nil => Nil
+    case x :: xs => reverse(xs) ::: List(x) 
+  }
+
+  def isPalindrome[T](list: List[T]): Boolean = list match {
+    case Nil => true
+    case x :: Nil => true
+    case x :: xs => if (x == xs.last ) isPalindrome(list.tail.init) else false
+  }
+
+  def flatten(list: List[Any]): List[Any] = {
+    def aux(elem: Any): List[Any] = elem match {
+      case Nil => Nil
+      case x :: xs => aux(x) ::: aux(xs)
+      case x => List(x)
+    }
+
+    aux(list)
+  }
+
+  def compress[T](list: List[T]): List[T] = {
+    def aux(curr: T, l: List[T]): List[T] = l match {
+      case Nil => Nil
+      case x :: Nil => if(x == curr) Nil else List(x)
+      case x :: xs => if(x == curr) aux(curr, xs) else x :: aux(x,xs)
+    }
+
+    if(list.isEmpty) Nil
+    else list.head :: aux(list.head, list.tail)
+  }
+  def pack[T](list: List[T]): List[List[T]] = {
+    def aux (l: List[T], curr: List[T]): List[List[T]] = (l, curr) match {
+      case (Nil,_) => List(curr)
+      case (x :: xs, c :: cs) =>
+        if(x == c) aux(xs, x :: curr )
+        else curr :: aux(xs, List(x))
+
+    }
+
+    if(list.isEmpty) Nil
+    else aux(list.tail, List(list.head))
+  }
   def encode[T](list: List[T]): List[(Int, T)] = ???
   def encodeModified[T](list: List[T]): List[Any] = ???
   def decode[T](list: List[(Int, T)]): List[T] = ???
