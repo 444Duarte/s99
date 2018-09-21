b7079c72f1485ba5ebd73aa038018ef5d2f26772
Duarte Pinto <duartemr.pinto@gmail.com>
2018-09-21 12:29:29 +0100

    p41 finished. Arithmetic exercises finished

--- src/main/scala/s99/ArithmeticSolutions.scala
+++ src/main/scala/s99/ArithmeticSolutions.scala
@@ -24,19 +24,42 @@ trait ArithmeticSolutions {
     def primeFactorMultiplicity: List[(Int, Int)] = new ListsSolutions {}.encodeDirect(this.primeFactors)
 
     def primeFactorMultiplicityMap: Map[Int, Int] = ???
+
     def improvedTotient: Int = n.primeFactorMultiplicity.map {
       case (p, m) => (p - 1) * math.pow(p, m - 1).toInt
     }.product
 
     def listPrimesinRange(r: Range): List[Int] = ???
-    def goldbach: (Int, Int) = ???
+    def goldbach: (Int, Int) = {
+      val f: List[(Int, Int)] =
+        for {
+          x <- listPrimesinRange(1 until n)
+          y <- listPrimesinRange(1 until n)
+          if x+y == n
+        } yield (x,y)
+
+      f.head
+    }
+
   }
 
   def gcd(m: Int, n: Int): Int = if (n == 0) m else gcd(n, m%n)
 
-  def listPrimesinRange(r: Range): List[Int] = ???
-  def printGoldbachList(r: Range): List[String] = ???
-  def printGoldbachListLimited(r: Range, limit: Int): List[String] = ???
+  def listPrimesinRange(r: Range): List[Int] = (for ( i <- r if i.isPrime) yield i).toList
+  def printGoldbachList(r: Range): List[String] =
+    for {
+      x <- listPrimesinRange(r)
+      y <- listPrimesinRange(r.min until x)
+      str <- x+y + " = $x + $y"
+    } yield str.toString
+
+  def printGoldbachListLimited(r: Range, limit: Int): List[String] =
+    for {
+      x <- listPrimesinRange(r)
+      y <- listPrimesinRange(r.min until x)
+      if x > 50 && y > 50
+      str <- x+y + " = $x + $y"
+    } yield str.toString
 
   // Optional but possibly useful exercise: not in original s-99 problems
   def primes: Stream[Int] = ???
